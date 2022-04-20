8a08e7a954ceaf21fdb1e52e5184f6dff6d6fcb0
Codefident <codefi@example.com>
2022-04-20 03:31:24 +0200

    added fading in

---
+++ src/components/WelcomeBanner/WelcomeBanner.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './WelcomeBanner.css';
+
+export default function WelcomeBanner() {
+    return(
+        <header className="Banner-header">Hi, I'm <b className="Banner-header-nickname">Codefident</b>, welcome to my website!</header>
+    );
+}
